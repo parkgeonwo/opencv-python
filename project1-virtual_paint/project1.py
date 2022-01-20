c5526bc438bc0b5015c65f2db75d327a938a7b61
Geonwoo Park <parkkeonwo@naver.com>
2022-01-20 23:34:38 +0900

    220120 23:34

--- project1-virtual_paint/project1.py
+++ project1-virtual_paint/project1.py
@@ -21,10 +21,13 @@ myColorValues = [[0,0,204],           # BGR / red
 [255,0,51],      # blue
 [0,255,255]]         # yellow
 
+myPoints = []          # [x, y, colorId]
+
 
 def findColor(img, myColors,myColorValues):     # color 찾는 함수
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)       # ch 7 참고 / bgr -> hsv로
     count = 0
+    newPoints = []
 
     for color in myColors: 
         lower = np.array(color[0:3])
@@ -32,8 +35,12 @@ def findColor(img, myColors,myColorValues):     # color 찾는 함수
         mask = cv2.inRange(imgHSV, lower, upper)   # img, lower limit, upper limit / 특정 범위 안에 있는 행렬 원소 검출
         x,y = getContours(mask)    # getCountours의 return 값을 x,y에 담는다.
         cv2.circle(imgResult,(x,y),10,myColorValues[count],cv2.FILLED)     # countours 의 특정지점에 circle 그림
+
+        if x!=0 and y!=0:
+            newPoints.append([x,y,count])
         count += 1
         # cv2.imshow(str(color[0]),mask)
+    return newPoints
 
 def getContours(img):    # ch8. 참고 # contours를 얻고 점을 얻기 위한 함수 생성
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)   # 윤곽선과 계층구조를 반환
@@ -57,10 +64,23 @@ def getContours(img):    # ch8. 참고 # contours를 얻고 점을 얻기 위한
             x,y,w,h = cv2.boundingRect(approx)  # 주어진 점을 감싸는 최소 크기 사각형(바운딩박스)를 반환합니다.
     return x+w//2,y
 
+def drawOnCanvas(myPoints, myColorValues):
+    for point in myPoints:
+        cv2.circle(imgResult,(point[0],point[1]),10,myColorValues[point[2]],cv2.FILLED)
+
+
+
 while True:
     success, img = cap.read()        # 재생되는 비디오의 한 프레임씩 읽습니다. 제대로 읽으면 success에 True 리턴 / 읽은 프레임은 frame에 리턴
     imgResult = img.copy()           # img 복사해서 윤곽선따기 위한 복사본
-    findColor(img, myColors,myColorValues)         # color 찾기
+    
+    newPoints = findColor(img, myColors,myColorValues)         # color 찾기
+    if len(newPoints) != 0:
+        for newP in newPoints:
+            myPoints.append(newP)
+    if len(myPoints) != 0:
+        drawOnCanvas(myPoints,myColorValues)
+
     cv2.imshow("Result",imgResult)          # 화면에 표시
     if cv2.waitKey(1) & 0xFF == ord('q'):    # 프레임 넘어가는 속도 1ms / q를 누르면 꺼지도록 설정
         break
